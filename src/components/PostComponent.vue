2c3c05eadf4f3fa954cbd5404af0b3058e3b49cc
timokluser-dev <timo@timokluser.ch>
2021-12-23 11:14:52 +0100

    add masonry grid effect

--- src/components/PostComponent.vue
+++ src/components/PostComponent.vue
@@ -1,17 +1,17 @@
 <template>
-  <div class="card">
+  <div class="card m-1">
     <div class="card-header">
       <h5 class="mb-0">{{ post.category.name }}</h5>
     </div>
-    <img class="card-img-top" alt="Post image" v-if="image" :src="image" />
+    <img class="card-img" alt="Post image" v-if="image" :src="image"/>
     <div class="card-body">
       <h3 class="card-title">{{ post.name }}</h3>
       <p class="card-text mb-2">{{ post.text }}</p>
 
       <span
-        class="badge rounded-pill bg-dark me-2"
-        v-for="keyword in post.keywords"
-        :key="keyword.id"
+          class="badge rounded-pill bg-dark me-2"
+          v-for="keyword in post.keywords"
+          :key="keyword.id"
       >
         {{ keyword.name }}
       </span>
@@ -33,4 +33,5 @@ export default class PostComponent extends Vue {
 }
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+</style>
--- src/containers/PostsContainer.vue
+++ src/containers/PostsContainer.vue
@@ -1,7 +1,7 @@
 <template>
   <div v-if="!loading && posts">
-    <div class="posts row row-cols-1 row-cols-md-3 g-4">
-      <div class="col" v-for="post in posts" :key="post.id">
+    <div class="posts row">
+      <div class="col-sm-6 col-lg-4 mb-4" v-for="post in posts" :key="post.id">
         <PostComponent :post="post"></PostComponent>
       </div>
     </div>
@@ -12,6 +12,7 @@
 import {Component, Vue} from 'vue-property-decorator';
 import {PostType} from '@/api/types/backend';
 import PostComponent from '@/components/PostComponent.vue';
+import Masonry from 'masonry-layout';
 
 @Component({
   components: {
@@ -19,8 +20,9 @@ import PostComponent from '@/components/PostComponent.vue';
   },
 })
 export default class PostsContainer extends Vue {
-  mounted(): void {
-    this.$store.dispatch('backend/fetchPosts');
+  async mounted(): Promise<void> {
+    await this.$store.dispatch('backend/fetchPosts');
+    new Masonry('.posts', {});
   }
 
   get loading(): boolean {
