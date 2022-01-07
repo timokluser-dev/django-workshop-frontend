fd58dc6a4e3e778853d1116373f7f76feb549682
timokluser-dev <timo@timokluser.ch>
2022-01-07 09:54:19 +0100

    update to `@apollo/client`

--- src/api/client/index.ts
+++ src/api/client/index.ts
@@ -1,17 +1,18 @@
-import {ApolloClient} from 'apollo-client';
-import {createHttpLink} from 'apollo-link-http';
-import {InMemoryCache} from 'apollo-cache-inmemory';
+import {ApolloClient} from '@apollo/client/core';
+import {InMemoryCache} from '@apollo/client/core';
+import {ApolloLink} from '@apollo/client/core';
+import {createUploadLink} from 'apollo-upload-client';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function createApolloClient(): ApolloClient<any> {
-  const httpLink = createHttpLink({
+  const httpLink = createUploadLink({
     uri: process.env.VUE_APP_GRAPHQL_URI,
   });
 
   const cache = new InMemoryCache();
 
   return new ApolloClient({
-    link: httpLink,
+    link: ApolloLink.from([httpLink]),
     cache: cache,
   });
 }
